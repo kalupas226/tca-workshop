0e4cb02087eac9ffe92b7f9690130f89b653f482
kalupas226 <kalupas226@gmail.com>
2023-10-09 20:55:12 +0900

    wip

--- Sources/RepositoryListFeature/RepositoryListView.swift
+++ Sources/RepositoryListFeature/RepositoryListView.swift
@@ -1,7 +1,8 @@
 import ComposableArchitecture
+import Dependencies
 import Entity
-import GitHubAPIClient
-import RepositoryDetailFeature
+import Foundation
+import IdentifiedCollections
 import SwiftUI
 
 public struct RepositoryList: Reducer {
@@ -9,80 +10,60 @@ public struct RepositoryList: Reducer {
     var repositoryRows: IdentifiedArrayOf<RepositoryRow.State> = []
     var isLoading: Bool = false
     @BindingState var query: String = ""
-    @PresentationState var alert: AlertState<Action.Alert>?
-    var path = StackState<Path.State>()
 
     public init() {}
   }
 
   public enum Action: Equatable, BindableAction {
     case onAppear
-    case queryChangeDebounced
     case searchRepositoriesResponse(TaskResult<[Repository]>)
-    case repositoryRows(id: RepositoryRow.State.ID, action: RepositoryRow.Action)
+    case repositoryRow(id: RepositoryRow.State.ID, action: RepositoryRow.Action)
+    case queryChangeDebounced
     case binding(BindingAction<State>)
-    case alert(PresentationAction<Alert>)
-    case path(StackAction<Path.State, Path.Action>)
-
-    public enum Alert: Equatable {}
   }
 
-  @Dependency(\.continuousClock) var clock
-  @Dependency(\.gitHubAPIClient) var gitHubAPIClient
-  
   public init() {}
 
   private enum CancelID {
     case response
   }
 
+  @Dependency(\.gitHubAPIClient) var gitHubAPIClient
+  @Dependency(\.mainQueue) var mainQueue
+
   public var body: some ReducerOf<Self> {
     BindingReducer()
-    Reduce<State, Action> { state, action in
+    Reduce { state, action in
       switch action {
       case .onAppear:
         state.isLoading = true
-
-        return .run { send in
-          await send(
-            .searchRepositoriesResponse(
-              TaskResult {
-                try await gitHubAPIClient.searchRepositories("composable")
-              }
-            )
-          )
-        }
+        return searchRepositories(by: "composable")
       case let .searchRepositoriesResponse(result):
         state.isLoading = false
 
         switch result {
-        case let .success(repositories):
+        case let .success(response):
           state.repositoryRows = .init(
-            uniqueElements: repositories.map {
+            uniqueElements: response.map {
               .init(repository: $0)
             }
           )
           return .none
         case .failure:
-          state.alert = .networkError
+          // TODO: Handling error
           return .none
         }
-      case let .repositoryRows(id: _, action: .delegate(.openRepositoryDetail(repository))):
-        state.path.append(
-          .repositoryDetail(
-            .init(repository: repository)
-          )
-        )
-        return .none
-      case .repositoryRows:
+      case .repositoryRow:
         return .none
       case .binding(\.$query):
         return .run { send in
-          try await withTaskCancellation(id: CancelID.response, cancelInFlight: true) {
-            try await clock.sleep(for: .seconds(0.3))
-            await send(.queryChangeDebounced)
-          }
+          await send(.queryChangeDebounced)
         }
+        .debounce(
+          id: CancelID.response,
+          for: .seconds(0.3),
+          scheduler: mainQueue
+        )
       case .queryChangeDebounced:
         guard !state.query.isEmpty else {
           return .none
@@ -90,49 +71,26 @@ public struct RepositoryList: Reducer {
 
         state.isLoading = true
 
-        return .run { [query = state.query] send in
-          await send(
-            .searchRepositoriesResponse(
-              TaskResult {
-                try await gitHubAPIClient.searchRepositories(query)
-              }
-            )
-          )
-        }
-      case .binding, .alert, .path:
+        return searchRepositories(by: state.query)
+      case .binding:
         return .none
       }
     }
-    .forEach(\.repositoryRows, action: /Action.repositoryRows(id:action:)) {
+    .forEach(\.repositoryRows, action: /Action.repositoryRow(id:action:)) {
       RepositoryRow()
     }
-    .forEach(\.path, action: /Action.path) {
-      Path()
-    }
   }
 
-  public struct Path: Reducer {
-    public enum State: Equatable {
-      case repositoryDetail(RepositoryDetail.State)
+  func searchRepositories(by query: String) -> Effect<Action> {
+    .run { send in
+      await send(
+        .searchRepositoriesResponse(
+          TaskResult {
+            try await gitHubAPIClient.searchRepositories(query)
+          }
+        )
+      )
     }
-
-    public enum Action: Equatable {
-      case repositoryDetail(RepositoryDetail.Action)
-    }
-
-    public var body: some ReducerOf<Self> {
-      Scope(state: /State.repositoryDetail, action: /Action.repositoryDetail) {
-        RepositoryDetail()
-      }
-    }
-  }
-}
-
-extension AlertState where Action == RepositoryList.Action.Alert {
-  static let networkError = Self {
-    TextState("Network Error")
-  } message: {
-    TextState("Failed to fetch data.")
   }
 }
 
@@ -144,12 +102,7 @@ public struct RepositoryListView: View {
   }
 
   public var body: some View {
-    NavigationStackStore(
-      store.scope(
-        state: \.path,
-        action: { .path($0) }
-      )
-    ) {
+    NavigationStack {
       WithViewStore(store, observe: { $0 }) { viewStore in
         Group {
           if viewStore.isLoading {
@@ -159,7 +112,7 @@ public struct RepositoryListView: View {
               ForEachStore(
                 store.scope(
                   state: \.repositoryRows,
-                  action: RepositoryList.Action.repositoryRows(id:action:)
+                  action: { .repositoryRow(id: $0, action: $1) }
                 ),
                 content: RepositoryRowView.init(store:)
               )
@@ -167,35 +120,20 @@ public struct RepositoryListView: View {
           }
         }
         .onAppear {
-          store.send(.onAppear)
+          viewStore.send(.onAppear)
         }
-        .navigationTitle("Search Repositories")
-        .alert(
-          store: store.scope(
-            state: \.$alert,
-            action: { .alert($0) }
-          )
-        )
+        .navigationTitle("Repositories")
         .searchable(
           text: viewStore.$query,
           placement: .navigationBarDrawer,
           prompt: "Input query"
         )
       }
-    } destination: {
-      switch $0 {
-      case .repositoryDetail:
-        CaseLet(
-          /RepositoryList.Path.State.repositoryDetail,
-           action: RepositoryList.Path.Action.repositoryDetail,
-           then: RepositoryDetailView.init(store:)
-        )
-      }
     }
   }
 }
 
-#Preview("API Succeeded") {
+#Preview {
   RepositoryListView(
     store: .init(
       initialState: RepositoryList.State()
@@ -203,24 +141,8 @@ public struct RepositoryListView: View {
       RepositoryList()
     } withDependencies: {
       $0.gitHubAPIClient.searchRepositories = { _ in
-        (1...20).map { .mock(id: $0) }
-      }
-    }
-  )
-}
-
-#Preview("API Failed") {
-  enum PreviewError: Error {
-    case fetchFailed
-  }
-  return RepositoryListView(
-    store: .init(
-      initialState: RepositoryList.State()
-    ) {
-      RepositoryList()
-    } withDependencies: {
-      $0.gitHubAPIClient.searchRepositories = { _ in
-        throw PreviewError.fetchFailed
+        try await Task.sleep(for: .seconds(0.3))
+        return (1...20).map { .mock(id: $0) }
       }
     }
   )
--- Tests/RepositoryListFeatureTests/RepositoryListFeatureTests.swift
+++ Tests/RepositoryListFeatureTests/RepositoryListFeatureTests.swift
@@ -6,7 +6,7 @@ import XCTest
 
 @MainActor
 final class RepositoryListFeatureTests: XCTestCase {
-  func testOnAppear_SeachSucceeded() async {
+  func testOnAppear_SearchSucceeded() async {
     let response: [Repository] = (1...10).map {
       .mock(id: $0)
     }
@@ -16,9 +16,11 @@ final class RepositoryListFeatureTests: XCTestCase {
     ) {
       RepositoryList()
     } withDependencies: {
-      $0.gitHubAPIClient.searchRepositories = { _ in response }
+      $0.gitHubAPIClient.searchRepositories = { _ in
+        response
+      }
     }
-    
+
     await store.send(.onAppear) {
       $0.isLoading = true
     }
@@ -32,45 +34,25 @@ final class RepositoryListFeatureTests: XCTestCase {
     }
   }
   
-  func testOnAppear_SearchFailed() async {
-    let store = TestStore(
-      initialState: RepositoryList.State()
-    ) {
-      RepositoryList()
-    } withDependencies: {
-      $0.gitHubAPIClient.searchRepositories = { _ in
-        throw TestError.search
-      }
-    }
-    
-    await store.send(.onAppear) {
-      $0.isLoading = true
-    }
-    await store.receive(.searchRepositoriesResponse(.failure(TestError.search))) {
-      $0.alert = .networkError
-      $0.isLoading = false
-    }
-  }
-
   func testQueryChanged() async {
     let response: [Repository] = (1...10).map {
       .mock(id: $0)
     }
-    let testClock = TestClock()
+    let testScheduler = DispatchQueue.test
 
     let store = TestStore(
       initialState: RepositoryList.State()
     ) {
       RepositoryList()
     } withDependencies: {
-      $0.continuousClock = testClock
       $0.gitHubAPIClient.searchRepositories = { _ in response }
+      $0.mainQueue = testScheduler.eraseToAnyScheduler()
     }
-
+    
     await store.send(.binding(.set(\.$query, "test"))) {
       $0.query = "test"
     }
-    await testClock.advance(by: .seconds(0.3))
+    await testScheduler.advance(by: .seconds(0.3))
     await store.receive(.queryChangeDebounced) {
       $0.isLoading = true
     }
@@ -83,30 +65,4 @@ final class RepositoryListFeatureTests: XCTestCase {
       $0.isLoading = false
     }
   }
-
-  func testRepositoryRowTapped() async {
-    var state = RepositoryList.State()
-    state.repositoryRows.append(
-      .init(
-        repository: .mock(id: 1)
-      )
-    )
-    let store = TestStore(
-      initialState: state
-    ) {
-      RepositoryList()
-    }
-
-    await store.send(.repositoryRows(id: 1, action: .delegate(.openRepositoryDetail(.mock(id: 1))))) {
-      $0.path = .init(
-        [
-          .repositoryDetail(.init(repository: .mock(id: 1)))
-        ]
-      )
-    }
-  }
-}
-
-private enum TestError: Error {
-  case search
 }
