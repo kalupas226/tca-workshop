c7c5eee3c542aef18ff50388dd8c62489e5c2235
kalupas226 <kalupas226@gmail.com>
2024-02-23 23:22:34 +0900

    Fix 04-ImprovingPreviews

--- Sources/Docs/Documentation.docc/Resources/04-ImprovingPreviews/04-02-code-0001-previous.swift
+++ Sources/Docs/Documentation.docc/Resources/04-ImprovingPreviews/04-02-code-0001-previous.swift
@@ -5,19 +5,21 @@ import Foundation
 import IdentifiedCollections
 import SwiftUI
 
-public struct RepositoryList: Reducer {
+@Reducer
+public struct RepositoryList {
+  @ObservableState
   public struct State: Equatable {
     var repositoryRows: IdentifiedArrayOf<RepositoryRow.State> = []
     var isLoading: Bool = false
-    @BindingState var query: String = ""
+    var query: String = ""
 
     public init() {}
   }
 
-  public enum Action: Equatable, BindableAction {
+  public enum Action: BindableAction {
     case onAppear
-    case searchRepositoriesResponse(TaskResult<[Repository]>)
-    case repositoryRow(id: RepositoryRow.State.ID, action: RepositoryRow.Action)
+    case searchRepositoriesResponse(Result<[Repository]>)
+    case repositoryRow(IdentifiedActionOf<RepositoryRow>)
     case queryChangeDebounced
     case binding(BindingAction<State>)
   }
@@ -50,9 +52,9 @@ public struct RepositoryList: Reducer {
           // TODO: Handling error
           return .none
         }
-      case .repositoryRow:
+      case .repositoryRows:
         return .none
-      case .binding(\.$query):
+      case .binding(\.query):
         return .run { send in
           await send(.queryChangeDebounced)
         }
@@ -73,7 +75,7 @@ public struct RepositoryList: Reducer {
         return .none
       }
     }
-    .forEach(\.repositoryRows, action: /Action.repositoryRow(id:action:)) {
+    .forEach(\.repositoryRows, action: \.repositoryRows) {
       RepositoryRow()
     }
   }
@@ -82,7 +84,7 @@ public struct RepositoryList: Reducer {
     .run { send in
       await send(
         .searchRepositoriesResponse(
-          TaskResult {
+          Result {
           }
         )
       )
