bed084ddfaf9948b7b27c3265971f8cc6e00e2c8
kalupas226 <kalupas226@gmail.com>
2024-05-11 13:14:13 +0900

    Use unimplemented

--- Sources/BuildConfig/Client.swift
+++ Sources/BuildConfig/Client.swift
@@ -3,5 +3,12 @@ import DependenciesMacros
 
 @DependencyClient
 public struct BuildConfig {
-  public var gitHubPersonalAccessToken: () -> String = { "" }
+  public var gitHubPersonalAccessToken: () -> String = {
+    // Use `unimplemented` for https://github.com/pointfreeco/swift-dependencies/issues/183.
+    // If this bug is resolved, we will return the placeholder value directly.
+    unimplemented(
+      "\(Self.self).gitHubPersonalAccessToken",
+      placeholder: ""
+    )
+  }
 }
